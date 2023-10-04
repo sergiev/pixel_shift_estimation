d5b15162dd153474b7b2c8ab1571b2e6e2de7801
Sergiev <s@msergiev.ru>
2023-10-04 15:23:32 +0300

    repo init

---
+++ geo_tiling.py
@@ -0,0 +1,164 @@
+import math
+import os
+import random
+from copy import deepcopy
+from typing import Optional, Tuple
+
+import cv2
+import numpy as np
+import pandas as pd
+from osgeo import gdal
+from tqdm import tqdm
+
+# SONY RX-1 at 270m height
+WINDOW_WIDTH = 276.1714286
+WINDOW_HEIGHT = 183.6
+FPS = 25
+
+
+def get_center_xy(ds):
+    width = ds.RasterXSize
+    height = ds.RasterYSize
+    (
+        minx,
+        pixel_width_meters,
+        x_rotation,
+        maxy,
+        y_rotation,
+        cell_height_meters,
+    ) = ds.GetGeoTransform()
+    maxx = minx + width * pixel_width_meters + height * x_rotation
+    miny = maxy + width * y_rotation + height * cell_height_meters
+    return (minx + maxx) / 2, (miny + maxy) / 2
+
+
+def rectify(ds: gdal.Dataset) -> gdal.Dataset:
+    """useless and harmful"""
+    gt = list(ds.GetGeoTransform())
+    gt[1] = abs(gt[1])
+    gt[-1] = abs(gt[-1])
+    result = deepcopy(ds)
+    result.SetGeoTransform(gt)
+    return result
+
+
+def shift(x: float, y: float, v_min: float, v_max: float) -> Tuple[float, float]:
+    """random coordinate shift. For cartesian systems only."""
+    v = random.uniform(v_min, v_max)
+    dx = random.uniform(-v, v)
+    dy = math.sqrt(v**2 - dx**2)
+    if random.random() > 0.5:
+        dy = -dy
+    return x + dx, y + dy
+
+
+def geo_crop(
+    ds: gdal.Dataset,
+    center_x: float,
+    center_y: float,
+    window_x: float = WINDOW_WIDTH,
+    window_y: float = WINDOW_HEIGHT,
+    destination_path: str = "/vsimem/crop.tif",
+) -> gdal.Dataset:
+    """Crop ds by center and size args
+    center_x, center_y - center coordinates
+    window_x, window_y - window side lengths in same coordinate system as center_x, center_y
+    """
+    ulx = center_x - window_x / 2
+    uly = center_y + window_y / 2
+    brx = center_x + window_x / 2
+    bry = center_y - window_y / 2
+    window = (ulx, uly, brx, bry)
+    return gdal.Translate(destName=destination_path, srcDS=ds, projWin=window)
+
+
+def is_valid(img: np.ndarray) -> bool:
+    """Check if img contains nodata pixels.
+    Nodata is determined as zero in all channels.
+    CHW order only"""
+    return not np.any(img.sum(axis=0) == 0)
+
+
+def imwrite(dest: str, ds: gdal.Dataset, side_limit=6000) -> float:
+    img = ds.ReadAsArray()
+    img = img[:-1].swapaxes(0, -1)  # drop alpha channel
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    r = min(1, side_limit / max(img.shape))
+    if r < 1:
+        img = cv2.resize(
+            img, dst=None, dsize=None, fx=r, fy=r, interpolation=cv2.INTER_AREA
+        )
+    cv2.imwrite(dest, img)
+    return r
+
+
+def generate_samples(
+    ds: gdal.Dataset,
+    destination_directory: str,
+    n: Optional[int] = 1000,
+    v_min: float = 0,
+    v_max: float = 10 / FPS,
+    window_width: float = WINDOW_WIDTH,
+    window_height: float = WINDOW_HEIGHT,
+    max_tries: int = 10,
+) -> dict:
+    """Generate n samples from ds center with random shift"""
+    images_dir = os.path.join(destination_directory, "images")
+    os.makedirs(images_dir, exist_ok=True)
+    center_x, center_y = get_center_xy(ds)
+    _, pixel_width_meters, _, _, _, pixel_height_meters = ds.GetGeoTransform()
+    result = []
+    for i in tqdm(range(n)):
+        success = False
+        tries = 0
+        while tries < max_tries and not success:
+            nx, ny = shift(center_x, center_y, v_min, v_max)
+            candidate_ds = geo_crop(nx, ny, window_width, window_height)
+            candidate_img = candidate_ds.ReadAsArray()
+            success = is_valid(candidate_img)
+        if not success:
+            print("Early stop: too much failed takes to crop proper image")
+            return result
+        r = imwrite(os.path.join(images_dir, f"{i}.png"), candidate_ds)
+        shift_x = math.ceil(r * (nx - center_x) / pixel_width_meters)
+        shift_y = math.ceil(r * (ny - center_y) / pixel_height_meters)
+        center_x, center_y = nx, ny
+        result.append(
+            {
+                "meter_center_x": center_x,
+                "meter_center_y": center_y,
+                "pixel_shift_x": shift_x,
+                "pixel_shift_y": shift_y,
+            }
+        )
+    return result
+
+
+def main(src_path: str, destination_directory: str):
+    csv_path = os.path.join(destination_directory, "centers.csv")
+    ds = gdal.Open(src_path)
+    result = generate_samples(ds, destination_directory)
+    pd.DataFrame(result).to_csv(csv_path)
+
+
+def gdal_error_handler(err_class, err_num, err_msg):
+    errtype = {
+            gdal.CE_None:'None',
+            gdal.CE_Debug:'Debug',
+            gdal.CE_Warning:'Warning',
+            gdal.CE_Failure:'Failure',
+            gdal.CE_Fatal:'Fatal'
+    }
+    err_msg = err_msg.replace('\n',' ')
+    err_class = errtype.get(err_class, 'None')
+    print ('Error Number: %s' % (err_num))
+    print ('Error Type: %s' % (err_class))
+    print ('Error Message: %s' % (err_msg))
+
+if __name__=='__main__':
+
+    # install error handler
+    gdal.PushErrorHandler(gdal_error_handler)
+    src_path = "meters.tif"
+    dst = "geo_sampled_270"
+    main(src_path, dst)
---
+++ metrics.py
@@ -0,0 +1,85 @@
+from typing import Dict, Union
+
+import numpy as np
+import pandas as pd
+
+
+def to_coord(x: float, y: float) -> np.array:
+    return np.array([x, y])
+
+
+def distance(a: np.array, b: np.array) -> Union[float, np.ndarray]:
+    assert a.ndim == b.ndim
+    return np.linalg.norm(a - b, axis=-1)
+
+
+def pixel_error_per_meter(
+    a_meter: np.array,
+    b_meter: np.array,
+    real_pix_shift: np.array,
+    pred_pix_shift: np.array,
+) -> Union[float, np.ndarray]:
+    """How much pixel errors made per meter of real shift?"""
+    shift_meter = distance(a_meter, b_meter)
+    pix_error = distance(real_pix_shift, pred_pix_shift)
+    return pix_error / shift_meter
+
+
+def pixel_to_meter(pix: np.array, meters_in_pixel: np.array) -> np.ndarray:
+    return pix * meters_in_pixel
+
+
+def meter_error(
+    real_pix: np.array,
+    pred_pix: np.array,
+    meters_in_pixel: np.array,
+    relative: bool = False,
+) -> Union[float, np.ndarray]:
+    real_meter = pixel_to_meter(real_pix, meters_in_pixel)
+    pred_meter = pixel_to_meter(pred_pix, meters_in_pixel)
+    error = distance(real_meter, pred_meter)
+    if relative:
+        error /= np.linalg.norm(real_meter)
+    return error
+
+
+def qa_np(
+    origin_meters: np.ndarray,
+    real_pix_shift: np.ndarray,
+    pred_pix_shift: np.ndarray,
+    meters_in_pixel: np.ndarray,
+) -> Dict[str, np.ndarray]:
+    pixel_error = distance(real_pix_shift, pred_pix_shift)
+    ame = meter_error(real_pix_shift, pred_pix_shift, meters_in_pixel, relative=False)
+    rme = meter_error(real_pix_shift, pred_pix_shift, meters_in_pixel, relative=True)
+    ppm = pixel_error_per_meter(
+        a_meter=origin_meters[:-1],
+        b_meter=origin_meters[1:],
+        real_pix_shift=real_pix_shift,
+        pred_pix_shift=pred_pix_shift,
+    )
+    return {
+        "pixel_error": pixel_error,
+        "absolute_meter_error": ame,
+        "relative_meter_error": rme,
+        "pixel_error_per_meter": np.concatenate([[0], ppm]),
+    }
+
+
+def qa_pd(gt: pd.DataFrame, pred: pd.DataFrame, meta: pd.DataFrame) -> pd.DataFrame:
+    origin_meters = gt[["meter_center_x", "meter_center_y"]].to_numpy()
+    real_pix_shift = gt[["pixel_shift_x", "pixel_shift_y"]].to_numpy()
+    pred_pix_shift = pred[["pixel_shift_x", "pixel_shift_y"]].to_numpy()
+    meters_in_pixel = meta[["meters_in_pixel_x", "meters_in_pixel_y"]].to_numpy()
+    total = qa_np(origin_meters, real_pix_shift, pred_pix_shift, meters_in_pixel)
+    df = pd.DataFrame(np.hstack((total.values())), columns=total.keys())
+    return df
+
+
+def qa_csv(gt_path: str, pred_path: str, meta_path: str, dst_path: str) -> None:
+    df = qa_pd(
+        gt=pd.read_csv(gt_path),
+        pred=pd.read_csv(pred_path),
+        meta=pd.read_csv(meta_path),
+    )
+    df.to_csv(dst_path)
